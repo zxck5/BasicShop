ec32fcc1df0dfe36ba6aee33140dae13a23ad72c
Jiwon Jeong <ideagiver8967@gmail.com>
2022-10-28 16:06:30 -0400

    commit

---
+++ BookShop/src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,25 @@
+//package jpabook.jpashop;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@RestController
+////@Controller
+//public class HelloController {
+//    @GetMapping("hello")
+//    public String hello(Model model) {
+//        model.addAttribute("data","hello!!!");
+//        return "hello";
+//    }
+//    @GetMapping("hello2")
+//    public List<String> Hello2() {
+//        return Arrays.asList("server port 8080", "react port 3030");
+//    }
+//
+//
+//}
